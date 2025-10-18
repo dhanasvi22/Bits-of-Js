1787278a6553b0e1d8e646f735ffaf5dcc63a4e3
dhanasvi22 <shendedhanasvi@gmail.com>
2025-10-18 00:27:31 -0400

    Arithmatic oprator

--- datatype.js
+++ datatype.js
@@ -60,3 +60,5 @@ console.log("BigInt datatype:");
 
 
 
+
+
---
+++ js oprator.js
@@ -0,0 +1,13 @@
+//js oprator 
+// + - * / % ** ++ --
+
+let x = 5;
+let y = 4;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x--);
+console.log(x++);
+console.log(x**y);
+console.log(x%y);
