cf1aeb81e1048e078ff6cd040e69bfe6dd79aa37
dhanasvi22 <shendedhanasvi@gmail.com>
2025-10-18 00:11:21 -0400

    js datatype

---
+++ datatype.js
@@ -0,0 +1,34 @@
+//js has 8 datatypes
+//1. Number - mathematical values
+//2. String - sequence of characters
+//3. Boolean - true or false
+//4. Null - empty value 
+//5. Undefined - variable not assigned a value
+//6. Object - collection of key-value pairs
+//7. Symbol - unique and immutable value
+//8. BigInt - for large integers
+
+//Number datatype
+
+let num1 = 42;
+let num2 = 3.14;
+console.log("Number datatype:");
+console.log("Integer:", num1);
+console.log("Float:", num2);
+
+//String datatype
+
+let str = "Hello, Ruchaa!";
+console.log("String datatype:");
+console.log(str);
+
+//Boolean datatype
+
+let isJavaScriptFun = true;
+let isFishTasty = false;
+console.log("Boolean datatype:");
+console.log("Is JavaScript fun?", isJavaScriptFun);
+console.log("Is fish tasty?", isFishTasty); 
+
+
+
---
+++ variable.js
@@ -0,0 +1,21 @@
+//js variable
+
+//var variable
+//let variable
+//const variable
+
+//var variable is globally scoped
+var a=10;
+var b=20;
+console.log(a+b);
+
+//let variable is block scoped
+let c=30;
+let d=40;
+console.log(c+d);
+
+
+//const variable is block scoped and cannot be reassigned
+const e=50;
+const f=60;
+console.log(e+f);
