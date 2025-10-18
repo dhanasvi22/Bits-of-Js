d55119b98956469a09e67c53d5d980cd4d28fbee
dhanasvi22 <shendedhanasvi@gmail.com>
2025-10-18 00:43:44 -0400

    assignment oprator

--- js oprator.js
+++ js oprator.js
@@ -1,6 +1,6 @@
 //js oprator 
 // + - * / % ** ++ --
-
+//Arithmatic oprator
 let x = 5;
 let y = 4;
 console.log(x+y);
@@ -11,3 +11,13 @@ console.log(x--);
 console.log(x++);
 console.log(x**y);
 console.log(x%y);
+
+//assignment opratior
+// =	x = y	x = y	x = 5
+// +=	x += y	x = x + y	x = 15
+// -=	x -= y	x = x - y	x = 5
+// *=	x *= y	x = x * y	x = 50
+// **=	x **= y	x = x ** y	x = 100000
+// /=	x /= y	x = x / y	x = 2
+// %=	x %= y	x = x % y	x = 0
+// :	x: 45	size.x = 45	x = 45
