8e429c3b3bda46ecdea88b18d25265dd85aa49d7
dhanasvi22 <shendedhanasvi@gmail.com>
2025-10-21 00:02:09 -0400

    conditional statements

--- assignment opratior.js
+++ assignment opratior.js
@@ -14,3 +14,5 @@ console.log(b);
  var c=36;
  c-=6;
  console.log(c);
+
+ //
---
+++ conditional.js
@@ -0,0 +1,53 @@
+// This is a conditional statement
+
+// if conditin code excute hone ke bad block hoti hai
+
+let canFly = true;
+if(canFly)
+    {
+    console.log("I can fly");
+}
+
+// another example
+
+let age = 20;
+if(age >= 18)
+    {
+    console.log("You are eligible to vote");
+    }
+
+// nested if statement
+
+let marks = 85;
+if(marks >= 60)
+    
+{
+    if(marks >= 85)
+    {
+        console.log("You got A grade");
+    }
+}
+
+// if-else statement
+// use jb 1st condition false ho to dusri condition check ho
+
+let number = 10;
+if(number % 2 == 0)
+    {
+    console.log("Even number");
+    }
+else
+    {
+    console.log("Odd number");
+    }
+
+    //anather example
+
+let time = 4;
+if(time < 12)
+    {
+    console.log("Good morning");
+    }       
+    else{
+        console.log("Good afternoon");
+    }
