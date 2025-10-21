60e6acc57a89d5e3614223dd160897b874fdca31
dhanasvi22 <shendedhanasvi@gmail.com>
2025-10-20 22:49:08 -0400

    Sub

---
+++ assignment opratior.js
@@ -0,0 +1,16 @@
+//assignment oprator
+
+// = 
+
+let r= 10; // x = y	x = y	x = 5
+console.log(r);
+
+//+= addition assignment
+var b=20;
+b+=20;
+console.log(b);
+
+// -= substact and assign
+ var c=36;
+ c-=6;
+ console.log(c);
